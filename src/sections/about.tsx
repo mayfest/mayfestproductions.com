b50fc138eeb37721266bcc549fdb74ff8a2f7dae
davfu <daveyblazefoo@gmail.com>
2025-01-30 22:12:36 -0600

    started about section, playing around with tailwind O_O

---
+++ src/sections/about.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { sectionStyles } from '@/components/index';
+
+export default function About() {
+  return (
+    <section id="about" className={sectionStyles}>
+      <div className="text-center font-semibold text-4xl my-8">
+        <p className="font-medium text-2xl md:text-3xl text-white">
+          At Mayfest Productions,
+        </p>
+        <p className="font-bold text-5xl md:text-6xl text-white">
+          <span className="inline-block mx-2">WE</span>
+          <span className="inline-block mx-2">DO</span>
+          <span className="inline-block mx-2">DILLO</span>
+          <span className="inline-block mx-2">DAY.</span>
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center gap-8 my-8">
+      <p className="flex-1 text-white text-lg md:text-xl">
+        Mayfest Productions is a student organization at Northwestern University that plans 
+        and produces the nation's largest student-run music festival — Dillo Day, with over 
+        12,000 festival attendees. In addition to the festival, Mayfest hosts and promotes a
+        number of other programming events including Battle of the Bands and Battle of the DJs. 
+      </p>
+
+        <div>
+          <iframe
+            title="We Do Dillo Day"
+            src="https://player.vimeo.com/video/291397953"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen
+            className="w-80 h-44 md:w-96 md:h-56 rounded-lg shadow-lg"
+          ></iframe>
+        </div>
+      </div>
+    </section>
+  );
+}
