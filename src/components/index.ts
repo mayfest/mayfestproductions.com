6729a1c59502fc87333a724283eb9602a94dee44
davfu <daveyblazefoo@gmail.com>
2025-02-06 21:56:08 -0600

    needs to be cleaned up but mostly similar to what we have on dd.com

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,1 @@
-export const sectionStyles = "w-full max-w-5xl mx-auto py-16 px-4";
+export const sectionStyles = "w-full max-w-7xl mx-auto py-16 px-4";
--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -6,8 +6,8 @@ export default function About() {
   return (
     <section id="about" className={sectionStyles}>
       <div className="text-center font-semibold text-4xl my-8">
-        <p className="font-medium text-2xl md:text-3xl text-white">
-          At Mayfest Productions,
+        <p className="font-bold text-2xl md:text-3xl text-white">
+          At <span className='text-pink-300'>Mayfest Productions</span>,
         </p>
         <p className="font-bold text-5xl md:text-6xl text-purple-300">
           WE DO DILLO DAY
@@ -15,11 +15,11 @@ export default function About() {
       </div>
 
       <div className="flex flex-col md:flex-row items-center gap-8 my-8">
-      <p className="flex-1 text-white text-lg md:text-xl">
+      <p className="flex-1 text-white text-xl md:text-xl font-semibold">
         <span className='text-purple-300'>Mayfest Productions</span> is a student organization at Northwestern University that plans 
         and produces the nation's largest student-run music festival — <span className='text-purple-300'>Dillo Day</span>, with over 
-        12,000 festival attendees. In addition to the festival, Mayfest hosts and promotes a
-        number of other programming events including Battle of the Bands and Battle of the DJs. 
+        12,000 festival attendees. In addition to the festival, Mayfest hosts and promotes a number of other programming events including
+        Battle of the Bands and Battle of the DJs. 
       </p>
 
         <div>
